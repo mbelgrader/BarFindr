38b8b153eefbde3927b68bd8b2c3d85cf9f71522
Maria Belgrader <mbelgrader5@gmail.com>
2017-01-11 21:13:47 -0800

    Add tagging seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -21,3 +21,37 @@ Bar.create!(name: 'Library Bar',
             address: '562 Sutter St',
             lat: 37.7893385,
             lng: -122.4119542)
+
+Tag.create!(name: 'dive') # 1
+Tag.create!(name: 'club') # 2
+Tag.create!(name: 'lounge') # 3
+Tag.create!(name: 'hookah') # 4
+Tag.create!(name: 'live music') # 5
+Tag.create!(name: 'food') # 6
+Tag.create!(name: 'casual') # 7
+Tag.create!(name: 'sports') # 8
+Tag.create!(name: 'hipster') # 9
+Tag.create!(name: 'biker') # 10
+Tag.create!(name: 'pool') # 11
+
+Tagging.create!(bar_id: 1, tag_id: 3)
+Tagging.create!(bar_id: 1, tag_id: 7)
+Tagging.create!(bar_id: 1, tag_id: 8)
+
+Tagging.create!(bar_id: 2, tag_id: 1)
+Tagging.create!(bar_id: 2, tag_id: 5)
+Tagging.create!(bar_id: 3, tag_id: 6)
+Tagging.create!(bar_id: 2, tag_id: 7)
+Tagging.create!(bar_id: 2, tag_id: 10)
+
+Tagging.create!(bar_id: 3, tag_id: 3)
+Tagging.create!(bar_id: 3, tag_id: 5)
+Tagging.create!(bar_id: 3, tag_id: 6)
+Tagging.create!(bar_id: 3, tag_id: 7)
+Tagging.create!(bar_id: 3, tag_id: 11)
+
+Tagging.create!(bar_id: 4, tag_id: 3)
+Tagging.create!(bar_id: 4, tag_id: 6)
+Tagging.create!(bar_id: 4, tag_id: 7)
+Tagging.create!(bar_id: 4, tag_id: 9)
+Tagging.create!(bar_id: 3, tag_id: 11)
